c2a8974a71ca2df1ca32bb011f6a352660a21d2c
Bvg2 <bvgradeck@gmail.com>
2025-09-10 13:43:57 -0400

    adding files

---
+++ function.js
@@ -0,0 +1,15 @@
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var dropdown = this.nextElementSibling;
+        if (dropdown.style.maxHeight) {
+            dropdown.style.maxHeight = null;
+        } else {
+            dropdown.style.maxHeight = dropdown.scrollHeight + "px";
+        }
+    });
+}
+
